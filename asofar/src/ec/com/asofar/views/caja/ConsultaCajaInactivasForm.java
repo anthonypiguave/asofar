f4c48ca5ef3bdaeac3928f696f88a1dd320c9493
admin1 <admin1@admin1-All-Series>
2019-05-14 18:21:50 -0500

    CAJASI 14-05-19 18:21

--- asofar/src/ec/com/asofar/views/caja/ConsultaCajaInactivasForm.java
+++ asofar/src/ec/com/asofar/views/caja/ConsultaCajaInactivasForm.java
@@ -1,11 +1,9 @@
-
 package ec.com.asofar.views.caja;
 
 import ec.com.asofar.views.grupo.*;
+import ec.com.asofar.dao.PrGruposJpaController;
 import ec.com.asofar.dao.VeCajaJpaController;
-import ec.com.asofar.dto.SeEmpresa;
-import ec.com.asofar.dto.SeSucursal;
-import ec.com.asofar.dto.SeUsuarios;
+import ec.com.asofar.dto.PrGrupos;
 import ec.com.asofar.dto.VeCaja;
 import ec.com.asofar.util.EntityManagerUtil;
 import ec.com.asofar.util.Tablas;
@@ -18,7 +16,7 @@ import javax.swing.JOptionPane;
  *
  * @author admin1
  */
-public class ConsultaCajasForm extends javax.swing.JDialog {
+public class ConsultaCajaInactivasForm extends javax.swing.JDialog {
 
     List<VeCaja> lista;
     VeCaja vc;
@@ -26,28 +24,19 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
     int y, x;
     String valor = "";
 
-    public ConsultaCajasForm(java.awt.Frame parent, boolean modal) {
+    public ConsultaCajaInactivasForm(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
         cargarInformacion();
         setLocationRelativeTo(null);
 
     }
-
-    public ConsultaCajasForm(java.awt.Frame parent, boolean modal, SeUsuarios us, SeEmpresa em, SeSucursal su) {
-        super(parent, modal);
-        initComponents();
-        cargarInformacion();
-        setLocationRelativeTo(null);
-
-    }
-    
 
     private void cargarInformacion() {
         try {
 
             lista = vCaja.findVeCajaEntities();
-            Tablas.listarCajas(lista, tbCajas);
+            Tablas.listarCajasInactivas(lista, tbCaja);
         } catch (Exception e) {
 
         }
@@ -64,10 +53,8 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
         txtfiltro = new javax.swing.JTextField();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        tbCajas = new javax.swing.JTable();
-        btnagregarnuevo = new javax.swing.JButton();
+        tbCaja = new javax.swing.JTable();
         btnsalir = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setUndecorated(true);
@@ -78,7 +65,7 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
         jLabel1.setFont(new java.awt.Font("Ubuntu", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(254, 254, 254));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("CAJAS");
+        jLabel1.setText("CAJAS INACTIVAS");
         jLabel1.setOpaque(true);
         jLabel1.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
             public void mouseDragged(java.awt.event.MouseEvent evt) {
@@ -95,16 +82,6 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
         jLabel2.setText("BUSCAR:");
 
         txtfiltro.setFont(new java.awt.Font("Ubuntu", 0, 14)); // NOI18N
-        txtfiltro.addFocusListener(new java.awt.event.FocusAdapter() {
-            public void focusLost(java.awt.event.FocusEvent evt) {
-                txtfiltroFocusLost(evt);
-            }
-        });
-        txtfiltro.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtfiltroActionPerformed(evt);
-            }
-        });
         txtfiltro.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 txtfiltroKeyTyped(evt);
@@ -116,7 +93,7 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
 
         jPanel2.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true));
 
-        tbCajas.setModel(new javax.swing.table.DefaultTableModel(
+        tbCaja.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null}
             },
@@ -124,12 +101,12 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        tbCajas.addMouseListener(new java.awt.event.MouseAdapter() {
+        tbCaja.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mousePressed(java.awt.event.MouseEvent evt) {
-                tbCajasMousePressed(evt);
+                tbCajaMousePressed(evt);
             }
         });
-        jScrollPane1.setViewportView(tbCajas);
+        jScrollPane1.setViewportView(tbCaja);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -144,32 +121,11 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
-        btnagregarnuevo.setFont(new java.awt.Font("Ubuntu", 1, 12)); // NOI18N
-        btnagregarnuevo.setForeground(new java.awt.Color(45, 115, 45));
-        btnagregarnuevo.setText("AGREGAR");
-        btnagregarnuevo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnagregarnuevoActionPerformed(evt);
-            }
-        });
-
-        btnsalir.setBackground(new java.awt.Color(177, 45, 52));
         btnsalir.setFont(new java.awt.Font("Ubuntu", 1, 12)); // NOI18N
-        btnsalir.setForeground(new java.awt.Color(254, 254, 254));
         btnsalir.setText("SALIR");
         btnsalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnsalirActionPerformed(evt);
-            }
-        });
-
-        jButton1.setBackground(new java.awt.Color(1, 1, 1));
-        jButton1.setFont(new java.awt.Font("Ubuntu", 1, 12)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(254, 254, 254));
-        jButton1.setText("INACTIVOS");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
             }
         });
 
@@ -184,20 +140,14 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
                         .addContainerGap()
                         .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(110, 110, 110)
-                                .addComponent(btnagregarnuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(btnsalir, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(170, 170, 170)
-                                .addComponent(jLabel2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtfiltro, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(139, 139, 139)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtfiltro, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnsalir, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -210,12 +160,9 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
                     .addComponent(txtfiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnagregarnuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnsalir, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnsalir)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -242,26 +189,19 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
         setLocation(point.x - x, point.y - y);
     }//GEN-LAST:event_jLabel1MouseDragged
 
-    private void btnagregarnuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnagregarnuevoActionPerformed
-        setVisible(false);
-        AgregarGruposForm ga = new AgregarGruposForm(new javax.swing.JFrame(), true);
-        ga.setVisible(true);
-    }//GEN-LAST:event_btnagregarnuevoActionPerformed
-
     private void btnsalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnsalirActionPerformed
 
         int r = JOptionPane.showConfirmDialog(null, "¿Desea Regresar?", "", JOptionPane.YES_NO_OPTION);
 
         if (r == JOptionPane.YES_OPTION) {
-            dispose();
+            setVisible(false);
 
-//            asdfg12345
         } else {
 
         }
     }//GEN-LAST:event_btnsalirActionPerformed
 
-    private void tbCajasMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tbCajasMousePressed
+    private void tbCajaMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tbCajaMousePressed
 
 //        int id = 0;
 //        pg = null;
@@ -271,20 +211,13 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
 //                if ((tbGrupos.getValueAt(id, 1).toString().equals(lista.get(i).getNombre()))) {
 //                    pg = lista.get(i);
 //                    if (pg != null) {
-//                        ModificarGruposForm es = new ModificarGruposForm(new javax.swing.JFrame(), true, pg);
+//                        ModificarGruposInactivosForm es = new ModificarGruposInactivosForm(new javax.swing.JFrame(), true, pg);
 //                        es.setVisible(true);
 //                    }
 //                }
 //            }
 //        }
-    }//GEN-LAST:event_tbCajasMousePressed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        ConsultaCajaInactivasForm cgi = new ConsultaCajaInactivasForm(new javax.swing.JFrame(), true);
-        cgi.setVisible(true);
-
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_tbCajaMousePressed
 
     private void txtfiltroKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtfiltroKeyTyped
         char c = evt.getKeyChar();
@@ -297,16 +230,8 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
     private void txtfiltroKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtfiltroKeyReleased
 
         valor = txtfiltro.getText();
-        Tablas.filtro(valor, tbCajas);
+        Tablas.filtro(valor, tbCaja);
     }//GEN-LAST:event_txtfiltroKeyReleased
-
-    private void txtfiltroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtfiltroActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtfiltroActionPerformed
-
-    private void txtfiltroFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtfiltroFocusLost
-    
-    }//GEN-LAST:event_txtfiltroFocusLost
 
     /**
      * @param args the command line arguments
@@ -322,25 +247,81 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
-
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ConsultaCajasForm.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-
+            java.util.logging.Logger.getLogger(ConsultaCajaInactivasForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ConsultaCajasForm.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-
+            java.util.logging.Logger.getLogger(ConsultaCajaInactivasForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ConsultaCajasForm.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-
+            java.util.logging.Logger.getLogger(ConsultaCajaInactivasForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ConsultaCajasForm.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ConsultaCajaInactivasForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -409,7 +390,7 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                ConsultaCajasForm dialog = new ConsultaCajasForm(new javax.swing.JFrame(), true);
+                ConsultaCajaInactivasForm dialog = new ConsultaCajaInactivasForm(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
@@ -422,15 +403,13 @@ public class ConsultaCajasForm extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnagregarnuevo;
     private javax.swing.JButton btnsalir;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable tbCajas;
+    private javax.swing.JTable tbCaja;
     private javax.swing.JTextField txtfiltro;
     // End of variables declaration//GEN-END:variables
 }
