151c55c0709f2346640739ca34b975954d0c4b7c
Cristhian1327itsgg <46070977+Cristhian1327itsgg@users.noreply.github.com>
2019-07-10 13:47:34 -0500

    Cristhian Latorre  Ajustes prestaciones x uni

---
+++ asofar/src/ec/com/asofar/daoext/In_PrestacionesPorUnidadServicioExt.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.asofar.daoext;
+
+import ec.com.asofar.dao.InPrestacionesPorServiciosJpaController;
+import ec.com.asofar.dto.InPrestacionesPorServicios;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Usuario
+ */
+public class In_PrestacionesPorUnidadServicioExt extends InPrestacionesPorServiciosJpaController {
+
+    public In_PrestacionesPorUnidadServicioExt(EntityManagerFactory emf) {
+        super(emf);
+    }
+
+    public List<InPrestacionesPorServicios> obtenerPrestNoRegis() {
+        EntityManager em = getEntityManager();
+        List<InPrestacionesPorServicios> lprod = null;
+
+        String nativeQuery = "SELECT P.*"
+                + " FROM pr_productos P\n"
+                + "WHERE P.estado='A';";
+        Query query = em.createNativeQuery(nativeQuery, InPrestacionesPorServicios.class);
+        lprod = query.getResultList();
+        return lprod;
+    }
+}
