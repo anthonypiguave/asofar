a27036f8bd2cfe6fc4486f61b8406e61dfc1f66d
Cristhian1327itsgg <46070977+Cristhian1327itsgg@users.noreply.github.com>
2019-08-10 13:58:03 -0500

    cristhian Venta DetalleMovimiento guardar verificar idbodega origen

---
+++ asofar/src/ec/com/asofar/daoext/InKardexExt.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.asofar.daoext;
+
+import ec.com.asofar.dao.InKardexJpaController;
+import ec.com.asofar.dto.InKardex;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Usuario
+ */
+public class InKardexExt extends InKardexJpaController {
+
+    public InKardexExt(EntityManagerFactory emf) {
+        super(emf);
+    }
+
+    public List<InKardex> obtenerProducto(Long id) {
+        EntityManager em = getEntityManager();
+        List<InKardex> lprod = null;
+
+//        String nativeQuery = "SELECT P.*"
+//                + " FROM pr_productos P\n"
+//                + "WHERE P.estado='A';";
+        String nativeQuery = "SELECT *\n"
+                + "FROM `in_kardex` \n"
+                + "WHERE id_producto = " + id +" "
+                + " AND fecha_movimiento=(SELECT MAX(fecha_movimiento)FROM `in_kardex`)";
+        Query query = em.createNativeQuery(nativeQuery, InKardex.class);
+        lprod = query.getResultList();
+        return lprod;
+    }
+}
