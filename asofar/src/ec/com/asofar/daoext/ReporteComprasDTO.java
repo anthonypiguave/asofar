1278a9be778d91692d325e969749c018c5c22220
admin1 <admin1@admin1-All-Series>
2019-08-12 16:02:54 -0500

    juan aristides ñ

---
+++ asofar/src/ec/com/asofar/daoext/ReporteComprasDTO.java
@@ -0,0 +1,395 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.asofar.daoext;
+
+import java.util.Date;
+
+/**
+ *
+ * @author usuario
+ */
+public class ReporteComprasDTO {
+
+    public Long id_orden_compra;
+    public Long id_empresa; 
+    public Long id_sucursal;
+    public Long id_proveedor;    
+    public Long id_tipo_documento;
+    public String observacion;
+    public Date fecha_entrega;
+    public Double total_subtotal; 
+    public Double total_descuento;
+    public Double total_ice;
+    public Double total_iva;
+    public Double total_compra;
+    public Date fecha_aprobacion;
+    public Long usuario_creacion;
+    public Date fecha_creacion;
+    public String usuario_actualizacion;
+    public Date fecha_actualizacion;
+    public String estado;
+        
+    public Long id_detalle_orden_compra;
+    public Long id_orden_comprad;
+    public Long id_empresa2;
+    public Long id_surcusal;
+    public Long linea_detalle;
+    public Long id_producto;
+    public String descripcion;
+    public Double precio_unitario;
+    public Long cantidad_recibida;
+    public Double subtotal;
+    public Double iva;
+    public Double ice;
+    public Double descuento;
+    public Double total;
+    public String estado2;
+    public Long usuario_creacion2;
+    public Date fecha_creacion2;
+    public String usuario_actualizacion2;
+    public Date fecha_actualizacion2;
+
+    public ReporteComprasDTO(Long id_orden_compra, Long id_empresa, Long id_sucursal, Long id_proveedor, Long id_tipo_documento, String observacion, Date fecha_entrega, Double total_subtotal, Double total_descuento, Double total_ice, Double total_iva, Double total_compra, Date fecha_aprobacion, Long usuario_creacion, Date fecha_creacion, String usuario_actualizacion, Date fecha_actualizacion, String estado, Long id_detalle_orden_compra, Long id_orden_comprad, Long id_empresa2, Long id_surcusal, Long linea_detalle, Long id_producto, String descripcion, Double precio_unitario, Long cantidad_recibida, Double subtotal, Double iva, Double ice, Double descuento, Double total, String estado2, Long usuario_creacion2, Date fecha_creacion2, String usuario_actualizacion2, Date fecha_actualizacion2) {
+        this.id_orden_compra = id_orden_compra;
+        this.id_empresa = id_empresa;
+        this.id_sucursal = id_sucursal;
+        this.id_proveedor = id_proveedor;
+        this.id_tipo_documento = id_tipo_documento;
+        this.observacion = observacion;
+        this.fecha_entrega = fecha_entrega;
+        this.total_subtotal = total_subtotal;
+        this.total_descuento = total_descuento;
+        this.total_ice = total_ice;
+        this.total_iva = total_iva;
+        this.total_compra = total_compra;
+        this.fecha_aprobacion = fecha_aprobacion;
+        this.usuario_creacion = usuario_creacion;
+        this.fecha_creacion = fecha_creacion;
+        this.usuario_actualizacion = usuario_actualizacion;
+        this.fecha_actualizacion = fecha_actualizacion;
+        this.estado = estado;
+        this.id_detalle_orden_compra = id_detalle_orden_compra;
+        this.id_orden_comprad = id_orden_comprad;
+        this.id_empresa2 = id_empresa2;
+        this.id_surcusal = id_surcusal;
+        this.linea_detalle = linea_detalle;
+        this.id_producto = id_producto;
+        this.descripcion = descripcion;
+        this.precio_unitario = precio_unitario;
+        this.cantidad_recibida = cantidad_recibida;
+        this.subtotal = subtotal;
+        this.iva = iva;
+        this.ice = ice;
+        this.descuento = descuento;
+        this.total = total;
+        this.estado2 = estado2;
+        this.usuario_creacion2 = usuario_creacion2;
+        this.fecha_creacion2 = fecha_creacion2;
+        this.usuario_actualizacion2 = usuario_actualizacion2;
+        this.fecha_actualizacion2 = fecha_actualizacion2;
+    }
+
+    public ReporteComprasDTO() {
+    }
+
+    public Long getId_orden_compra() {
+        return id_orden_compra;
+    }
+
+    public void setId_orden_compra(Long id_orden_compra) {
+        this.id_orden_compra = id_orden_compra;
+    }
+
+    public Long getId_empresa() {
+        return id_empresa;
+    }
+
+    public void setId_empresa(Long id_empresa) {
+        this.id_empresa = id_empresa;
+    }
+
+    public Long getId_sucursal() {
+        return id_sucursal;
+    }
+
+    public void setId_sucursal(Long id_sucursal) {
+        this.id_sucursal = id_sucursal;
+    }
+
+    public Long getId_proveedor() {
+        return id_proveedor;
+    }
+
+    public void setId_proveedor(Long id_proveedor) {
+        this.id_proveedor = id_proveedor;
+    }
+
+    public Long getId_tipo_documento() {
+        return id_tipo_documento;
+    }
+
+    public void setId_tipo_documento(Long id_tipo_documento) {
+        this.id_tipo_documento = id_tipo_documento;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public Date getFecha_entrega() {
+        return fecha_entrega;
+    }
+
+    public void setFecha_entrega(Date fecha_entrega) {
+        this.fecha_entrega = fecha_entrega;
+    }
+
+    public Double getTotal_subtotal() {
+        return total_subtotal;
+    }
+
+    public void setTotal_subtotal(Double total_subtotal) {
+        this.total_subtotal = total_subtotal;
+    }
+
+    public Double getTotal_descuento() {
+        return total_descuento;
+    }
+
+    public void setTotal_descuento(Double total_descuento) {
+        this.total_descuento = total_descuento;
+    }
+
+    public Double getTotal_ice() {
+        return total_ice;
+    }
+
+    public void setTotal_ice(Double total_ice) {
+        this.total_ice = total_ice;
+    }
+
+    public Double getTotal_iva() {
+        return total_iva;
+    }
+
+    public void setTotal_iva(Double total_iva) {
+        this.total_iva = total_iva;
+    }
+
+    public Double getTotal_compra() {
+        return total_compra;
+    }
+
+    public void setTotal_compra(Double total_compra) {
+        this.total_compra = total_compra;
+    }
+
+    public Date getFecha_aprobacion() {
+        return fecha_aprobacion;
+    }
+
+    public void setFecha_aprobacion(Date fecha_aprobacion) {
+        this.fecha_aprobacion = fecha_aprobacion;
+    }
+
+    public Long getUsuario_creacion() {
+        return usuario_creacion;
+    }
+
+    public void setUsuario_creacion(Long usuario_creacion) {
+        this.usuario_creacion = usuario_creacion;
+    }
+
+    public Date getFecha_creacion() {
+        return fecha_creacion;
+    }
+
+    public void setFecha_creacion(Date fecha_creacion) {
+        this.fecha_creacion = fecha_creacion;
+    }
+
+    public String getUsuario_actualizacion() {
+        return usuario_actualizacion;
+    }
+
+    public void setUsuario_actualizacion(String usuario_actualizacion) {
+        this.usuario_actualizacion = usuario_actualizacion;
+    }
+
+    public Date getFecha_actualizacion() {
+        return fecha_actualizacion;
+    }
+
+    public void setFecha_actualizacion(Date fecha_actualizacion) {
+        this.fecha_actualizacion = fecha_actualizacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Long getId_detalle_orden_compra() {
+        return id_detalle_orden_compra;
+    }
+
+    public void setId_detalle_orden_compra(Long id_detalle_orden_compra) {
+        this.id_detalle_orden_compra = id_detalle_orden_compra;
+    }
+
+    public Long getId_orden_comprad() {
+        return id_orden_comprad;
+    }
+
+    public void setId_orden_comprad(Long id_orden_comprad) {
+        this.id_orden_comprad = id_orden_comprad;
+    }
+
+    public Long getId_empresa2() {
+        return id_empresa2;
+    }
+
+    public void setId_empresa2(Long id_empresa2) {
+        this.id_empresa2 = id_empresa2;
+    }
+
+    public Long getId_surcusal() {
+        return id_surcusal;
+    }
+
+    public void setId_surcusal(Long id_surcusal) {
+        this.id_surcusal = id_surcusal;
+    }
+
+    public Long getLinea_detalle() {
+        return linea_detalle;
+    }
+
+    public void setLinea_detalle(Long linea_detalle) {
+        this.linea_detalle = linea_detalle;
+    }
+
+    public Long getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(Long id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio_unitario() {
+        return precio_unitario;
+    }
+
+    public void setPrecio_unitario(Double precio_unitario) {
+        this.precio_unitario = precio_unitario;
+    }
+
+    public Long getCantidad_recibida() {
+        return cantidad_recibida;
+    }
+
+    public void setCantidad_recibida(Long cantidad_recibida) {
+        this.cantidad_recibida = cantidad_recibida;
+    }
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Double getIva() {
+        return iva;
+    }
+
+    public void setIva(Double iva) {
+        this.iva = iva;
+    }
+
+    public Double getIce() {
+        return ice;
+    }
+
+    public void setIce(Double ice) {
+        this.ice = ice;
+    }
+
+    public Double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(Double descuento) {
+        this.descuento = descuento;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public String getEstado2() {
+        return estado2;
+    }
+
+    public void setEstado2(String estado2) {
+        this.estado2 = estado2;
+    }
+
+    public Long getUsuario_creacion2() {
+        return usuario_creacion2;
+    }
+
+    public void setUsuario_creacion2(Long usuario_creacion2) {
+        this.usuario_creacion2 = usuario_creacion2;
+    }
+
+    public Date getFecha_creacion2() {
+        return fecha_creacion2;
+    }
+
+    public void setFecha_creacion2(Date fecha_creacion2) {
+        this.fecha_creacion2 = fecha_creacion2;
+    }
+
+    public String getUsuario_actualizacion2() {
+        return usuario_actualizacion2;
+    }
+
+    public void setUsuario_actualizacion2(String usuario_actualizacion2) {
+        this.usuario_actualizacion2 = usuario_actualizacion2;
+    }
+
+    public Date getFecha_actualizacion2() {
+        return fecha_actualizacion2;
+    }
+
+    public void setFecha_actualizacion2(Date fecha_actualizacion2) {
+        this.fecha_actualizacion2 = fecha_actualizacion2;
+    }
+    
+    
+}
--- asofar/src/ec/com/asofar/daoext/ReporteriaExt.java
+++ asofar/src/ec/com/asofar/daoext/ReporteriaExt.java
@@ -6,13 +6,18 @@
 package ec.com.asofar.daoext;
 
 import ec.com.asofar.dto.CoOrdenCompras;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -30,17 +35,20 @@ public class ReporteriaExt {
 
     public static void main(String[] args) {
         try{
-        List<String> itemList = new ArrayList<String>();
-        itemList=reporteCompras(em);
-        
-            System.out.println("j "+itemList.add("id_orden_compra"));
+        List<ReporteComprasDTO1> itemList = new ArrayList<ReporteComprasDTO1>();
+        itemList = reporteCompras(em);
+      //  reporteCompras2(em);
+            //System.out.println("j "+itemList.add("id_orden_compra"));
             System.out.println("lalala");
             
-        String[] itemsArray = new String[itemList.size()];
-        itemsArray = itemList.toArray(itemsArray);
         
-        for(String s : itemsArray)
-            System.out.println(s);
+            for (ReporteComprasDTO1 str : itemList) {
+                System.out.println(str.getId_orden_compra());
+            }
+//            for (int i = 0; i < itemList.size(); i++) {
+//                  System.out.println(itemList.get(i).getFecha_entrega().toString());
+//            }
+//        
 //        Iterator<CoOrdenCompras> iter = lista.iterator();
 //        
 //        while (iter.hasNext()) {
@@ -55,21 +63,70 @@ public class ReporteriaExt {
         }
     }
 
-    public static List<String> reporteCompras(EntityManager em) {
-        List<String> listaCompra = new ArrayList<String>();
-        String nativeQuery = "SELECT * FROM `co_orden_compras` a\n"
-                + "INNER JOIN `co_detalle_orden_compra` b \n"
-                + "ON a.`id_orden_compra` = b.`id_orden_compra`\n"
-                + "AND a.`estado`='P'\n"
-                + "ORDER BY a.`fecha_creacion`";
-        Query query = em.createNativeQuery(nativeQuery);
-        listaCompra = query.getResultList();
-        // System.out.println("prueba numero de orden "+ id);
-//
-//        for (int i = 0; i < listaCompra.size(); i++) {
-//            System.out.println("fffff" + listaCompra.get(i).getUsuarioCreacion());
-//
+    public static List<ReporteComprasDTO1> reporteCompras(EntityManager em) {
+        List<ReporteComprasDTO1> listaCompra = new ArrayList<>();
+        List  obj4 =null;
+//        ArrayList<ReporteComprasDTO1> lista = new ArrayList<>();
+//        ReporteComprasDTO1 obj=null;
+//        System.out.println("jjjj");
+//       
+//          String nativeQuery = "SELECT id_orden_compra FROM co_orden_compras c";
+//          Query  query = em.createNativeQuery(nativeQuery);
+//          listaCompra = (ArrayList)query.getResultList();
+//          for (int i = 0; i < listaCompra.size(); i++) {
+//            //listaCompra = ((List)query.getResultList());
+//            obj.setId_orden_compra(listaCompra.get(i).getId_orden_compra());
+//            lista.add(obj);
 //        }
+//        
+//        for (ReporteComprasDTO1 f : lista) {
+//            System.out.println(f.getId_orden_compra());
+//        }
+                 
+          Query q = em.createNativeQuery("SELECT c.id_orden_compra,c.id_tipo_documento FROM co_orden_compras c");
+          for (int i = 0; i <  q.getResultList().size(); i++) {
+             ReporteComprasDTO1 obj = new ReporteComprasDTO1();
+//             obj.setId_orden_compra(Long.valueOf(q.getResultList().get(i).toString()));
+//             obj.setId_tipo_documento(Long.valueOf(q.getResultList().get(i).toString()));
+               obj4 = q.getResultList();
+           //   listaCompra.add(obj);
+              
+        }
+         
+         for (int j = 0; j < obj4.size(); j++) {
+                 ReporteComprasDTO1 obj = new ReporteComprasDTO1();
+                 obj.setId_orden_compra(Long.valueOf(obj4.get(j).toString()));
+                 listaCompra.add(obj);
+              }
+
+            for (ReporteComprasDTO1 a : listaCompra) {
+                System.out.println("Author "
+                        + a.getId_orden_compra().toString()
+                        + " "
+                        +a.getId_tipo_documento().toString());
+            }
         return listaCompra;
+    }
+    
+     private long showId(EntityManager em) {
+        String nativeQuery = "SELECT max(id_orden_compra) FROM co_orden_compras;";
+        Query query = em.createNativeQuery(nativeQuery);
+        long id = ((Number)query.getSingleResult()).longValue();
+        System.out.println("prueba numero de orden "+ id);
+        return id;
+        
+    }
+     
+    public static void reporteCompras2(EntityManager em) {
+        List<ReporteComprasDTO1> listaCompra = new ArrayList<>();
+      Query query = em.createNativeQuery(
+                "SELECT c.id_orden_compra,c.id_tipo_documento FROM co_orden_compras c", 
+                ReporteComprasDTO1.class);
+        for (int i = 0; i < query.getResultList().size(); i++) {
+            listaCompra = (List)query.getResultList().get(i);
+        }
+        for (ReporteComprasDTO1 r : listaCompra) {
+            System.out.println(r.getId_orden_compra().toString()+"  "+r.getId_tipo_documento().toString());
+        }
     }
 }
