032ce86bfef045a34577bbf363bd7f838e8ae8eb
admin1 <admin1@admin1-All-Series>
2019-08-15 17:17:09 -0500

    jefferson anchudia :D jejejeje puto el que lo lea!..

--- src/ec/com/asofar/views/reporteria/ReporteriaCompras.java
+++ src/ec/com/asofar/views/reporteria/ReporteriaCompras.java
@@ -20,10 +20,10 @@ import java.util.List;
  *
  * @author admin1
  */
-public class ReporteriaVenta extends javax.swing.JDialog {
+public class ReporteriaCompras extends javax.swing.JDialog {
 
     /**
-     * Creates new form ReporteriaVenta
+     * Creates new form ReporteriaCompras
      */
     SeUsuarios usu;
     SeEmpresa emp;
@@ -32,14 +32,14 @@ public class ReporteriaVenta extends javax.swing.JDialog {
     ReporteriaExt rep = new ReporteriaExt();
     List<ReporteComprasDTO> itemList = null;
     
-    public ReporteriaVenta(java.awt.Frame parent, boolean modal) {
+    public ReporteriaCompras(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
         itemList = rep.reporteCompras();
         Tablas.listarReporteCompras(itemList, tbaReporteCompra);
     }
 
-    public ReporteriaVenta(java.awt.Frame parent, boolean modal, SeUsuarios us, SeEmpresa em, SeSucursal su) {
+    public ReporteriaCompras(java.awt.Frame parent, boolean modal, SeUsuarios us, SeEmpresa em, SeSucursal su) {
         super(parent, modal);
         initComponents();
         usu = us;
@@ -319,21 +319,23 @@ public class ReporteriaVenta extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ReporteriaVenta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReporteriaCompras.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ReporteriaVenta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReporteriaCompras.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ReporteriaVenta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReporteriaCompras.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ReporteriaVenta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ReporteriaCompras.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                ReporteriaVenta dialog = new ReporteriaVenta(new javax.swing.JFrame(), true);
+                ReporteriaCompras dialog = new ReporteriaCompras(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
