ef6f7066c0678a92f11aebbcdb90a599c8bf5a80
admin1 <admin1@admin1-All-Series>
2019-09-02 18:32:28 -0500

    prestacioon actualiza

--- src/ec/com/asofar/views/prestaciones/ActualizarPrestacion.java
+++ src/ec/com/asofar/views/prestaciones/ActualizarPrestacion.java
@@ -189,7 +189,8 @@ public class ActualizarPrestacion extends javax.swing.JDialog {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        setVisible(false);
+this.setVisible(false);
+        
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
