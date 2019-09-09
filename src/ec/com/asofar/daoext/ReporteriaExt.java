e7eef547a1b9fb2ba35bcb52744e830046adac62
admin1 <admin1@admin1-All-Series>
2019-09-09 18:21:55 -0500

    jefferson reporte compra 1

--- src/ec/com/asofar/daoext/ReporteriaExt.java
+++ src/ec/com/asofar/daoext/ReporteriaExt.java
@@ -74,7 +74,7 @@ public class ReporteriaExt {
                 + "   FROM co_orden_compras as c\n"
                 + "   inner join co_detalle_orden_compra as d\n"
                 + "   	  on   c.id_orden_compra = d.id_orden_compra\n"
-                + "         and  c.estado = 'P'\n"
+                + "         and  c.estado = 'C'\n"
                 + "         and  c.fecha_entrega\n"
                 + "         BETWEEN concat(date_format(sysdate(),'%Y-%m-%d'),' 00:00:00')\n"
                 + "         and concat(date_format(sysdate(),'%Y-%m-%d'),' 23:59:59')\n"
