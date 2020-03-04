2f832fc3a312e40f80e35ede0f0d4a2c2918291c
ESTUDIANTE <ESTUDIANTE@usuario>
2020-03-04 16:56:22 -0500

    caja

--- src/ec/com/asofar/views/caja/Cierre_Caja.java
+++ src/ec/com/asofar/views/caja/Cierre_Caja.java
@@ -77,6 +77,7 @@ public class Cierre_Caja extends javax.swing.JDialog {
     
     public Cierre_Caja(java.awt.Frame parent, boolean modal, SeUsuarios se, SeEmpresa em, SeSucursal su) {
         super(parent, modal);
+        setUndecorated(true);
         initComponents();
         this.setLocationRelativeTo(null);
         seUsuario = se;
@@ -87,6 +88,7 @@ public class Cierre_Caja extends javax.swing.JDialog {
 
     public Cierre_Caja(java.awt.Frame parent, boolean modal, VeDetalleCaja veCaja, SeUsuarios se, SeEmpresa em, SeSucursal su) {
         super(parent, modal);
+        setUndecorated(true);
         initComponents();
         this.setLocationRelativeTo(null);
         seUsuario = se;
